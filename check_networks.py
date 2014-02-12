b0721044ebef6570d2a5659938c55029ba7fc7f8
HazCod <hazcod@outlook.be>
2014-02-12 14:04:50 +0100

    Made script add all channels into database.

--- check_networks.py
+++ check_networks.py
@@ -3,32 +3,36 @@
 import subprocess
 import socket
 
+from iwlistparse import *
 from database import *
 
-allinfo=subprocess.check_output(["sudo","iw", "wlan0", "scan", "passive"])
-
-tempfile = open ("/tmp/ssid", "wb")
-tempfile.write(allinfo)
-tempfile.close()
-
-list=[]
-tempfile= open ("/tmp/ssid", "r")
-for line in tempfile:
-	if "SSID" in line:
-		temp= (line.split(": "))
-		ssid= (temp[1]);
-		if (ssid.rstrip() not in list):
-			list.append(ssid.rstrip())
-tempfile.close()
-
-for ssid in list:
-	print (ssid)
-	query= 'insert into wnets values ("'
-	query+=ssid
+#lequery=''
+raw=getNetworks()
+for lijst in raw:
+	#print(lijst)
+	query='insert into ap_info(wifi_network,caption,quality,channel,mac_adress)  values ("';
+	query+=lijst['Name']
 	query+='","'
 	query+=socket.gethostname()
-	query+='",'
-	query+="now()"
-	query+=');'
-	print (query)
-	executequery(query)	
+	query+='","'
+	query+=lijst['Quality'][:-2][-1:]
+	query+='","'
+	query+=lijst['Channel']
+	query+='","'
+	query+=lijst['Address']
+	query+='"); '
+	lequery+=query
+	#print(query)
+	executequery(query)
+#executequery(lequery)
+# for ssid in list:
+# 	print (ssid)
+# 	query= 'insert into wnets values ("'
+# 	query+=ssid
+# 	query+='","'
+# 	query+=socket.gethostname()
+# 	query+='",'
+# 	query+="now()"
+# 	query+=');'
+# 	print (query)
+# 	executequery(query)	
