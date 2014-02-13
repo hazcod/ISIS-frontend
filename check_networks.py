2bac49a063114932e7c9e49b3d7ff72a7e97b96e
joost <joost.fock@gmail.com>
2014-02-13 15:05:23 +0100

    werkende check_networks

--- check_networks.py
+++ check_networks.py
@@ -1,34 +1,74 @@
-#! /usr/bin/python
+#!/usr/bin/python
 
 import subprocess
 import socket
-
 from database import *
 
-allinfo=subprocess.check_output(["sudo","iw", "wlan0", "scan", "passive"])
+allinfo= subprocess.check_output(["sudo", "iwlist", "wlan0","scan"])
 
-tempfile = open ("/tmp/ssid", "wb")
+tempfile= open("/tmp/ssid","wb")
 tempfile.write(allinfo)
 tempfile.close()
 
-list=[]
+networks=[]
+network={}
 tempfile= open ("/tmp/ssid", "r")
 for line in tempfile:
-	if "SSID" in line:
-		temp= (line.split(": "))
-		ssid= (temp[1]);
-		if (ssid.rstrip() not in list):
-			list.append(ssid.rstrip())
-tempfile.close()
+	if "Address" in line:
+		lineparts= line.split(":", 1)
+		MAC= lineparts[1].rstrip().lstrip()
+		network={"MAC": MAC}
+		networks.append(network)
+	if "Channel:" in line:
+		lineparts=line.split(":")
+		channel= lineparts[1].rstrip()
+		network["channel"]= channel
+	if "Quality" in line:
+		lineparts=line.split("=")
+		lineparts2=lineparts[1].split("/")
+		quality=lineparts2[0]
+		network["quality"]=quality
+	if "Encryption key" in line:
+		lineparts=line.split(":")
+		if lineparts[1].rstrip()=="off":
+			network["enc"]="open"
+	if "ESSID" in line:
+		lineparts=line.split(":")
+		SSID=lineparts[1].rstrip()
+		network["SSID"]= SSID
+	if "WPA" in line:
+		lineparts= line.split("/")
+		encPart=lineparts[1].rstrip();
+	if "Authentication Suites" in line:
+		lineparts=line.split(":")
+		encPart+=lineparts[1].rstrip()
+		network["enc"]= encPart
+	if "enc" not in network:
+		network["enc"]= "WEP"
 
-for ssid in list:
-	print (ssid)
-	query= 'insert into wnets values ("'
-	query+=ssid
-	query+='","'
+
+query= "delete from ap_info where caption='"
+query+=socket.gethostname()
+query+="';"
+executequery(query)
+query="insert into ap_info(wifi_network,caption,quality,channel,mac_adress,encryption, last_updated) values "
+for network in networks:
+	query+='('
+	query+=network["SSID"]
+	query+=',"'
 	query+=socket.gethostname()
 	query+='",'
+	query+=network["quality"]
+	query+=','
+	query+=network["channel"]
+	query+=',"'
+	query+=network["MAC"]
+	query+='","'
+	query+=network["enc"]
+	query+='",'
 	query+="now()"
-	query+=');'
-	print (query)
-	executequery(query)	
+	query+="),\n"
+query=query[:-2]
+query+=";"
+print (query)
+executequery(query)
