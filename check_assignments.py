34d5238f42a29185eaf620ae6d294cde2481ee33
joost <joost.fock@gmail.com>
2014-02-13 16:36:12 +0100

    BETTER assignment script

--- check_assignments.py
+++ check_assignments.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 import socket
+import subprocess
 from database import *
 
 query='update units set last_seen = now() where caption="'
@@ -8,10 +9,19 @@ query+='";'
 
 executequery(query)
 
-query='select * from assignments where caption="'
+query='select assignments_id from assignments where caption="'
 query+=socket.gethostname()
-query+='";'
+query+='"and status="new" order by 1 ASC limit 1;'
+ass_id= executequery(query)
+
+query='select assignment from assignments where caption="'
+query+=socket.gethostname()
+query+='"and status="new" order by 1 ASC limit 1;'
 assignments= executequery(query)
 
-for row in assignments:
-	print (row)
+if assignments[0][0] == "gitCheckout":
+	subprocess.Popen("/home/isis/git/checkout_frontendGit",shell=True);
+	query='update assignments SET status = "executed" where assignments_id ="'
+	query+=str(ass_id[0][0])
+	query+='";'
+	executequery(query) 
