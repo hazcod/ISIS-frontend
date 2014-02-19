ddf854a93ccb6da76e6b28924a74457c2d90f4a4
HazCod <hazcod@outlook.be>
2014-02-19 14:52:47 +0100

    Update take_image.py

--- take_image.py
+++ take_image.py
@@ -6,7 +6,7 @@ import re
 import os
 
 def take_picture():
-	result = subprocess.popen(['sudo raspistill', '-o' ,'/home/isis/ISIS-frontend/image.jpg']).read()
+	result = subprocess.Popen(['sudo raspistill', '-o' ,'/home/isis/ISIS-frontend/image.jpg']).read()
 	if "failed" not in result:
 		sleep("5")
 		os.system("scp  /home/isis/ISIS-frontend/image.jpg isis@192.168.255.54:/home/isis/Afbeeldingen/image.jpg")
