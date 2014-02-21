04490048f7d7b0aa8604d5e220093f62b8514847
Shadyxx <dylan.crokaert@gmail.com>
2014-02-21 10:40:00 +0100

    Update take_image.py

--- take_image.py
+++ take_image.py
@@ -10,7 +10,7 @@ def take_picture():
 	result = subprocess.check_output(['sudo', 'raspistill', '-o' ,'/home/isis/ISIS-frontend/image.jpg'])
 	if "failed" not in result:
 		time.sleep(5)
-		os.system("scp  /home/isis/ISIS-frontend/image.jpg isis@192.168.255.54:/home/isis/Afbeeldingen/image.jpg")
+		os.system("scp  /home/isis/ISIS-frontend/image.jpg isis@193.191.187.44:/home/isis/Afbeeldingen/image.jpg")
 	else:
 		raise Exception("Camera is not found! (or some other error)")
 
