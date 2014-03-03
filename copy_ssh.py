86be8ab6d4fda52bf6ba5ac809f201fd84e6d748
joost <joost.fock@gmail.com>
2014-03-03 15:55:49 +0100

    copy_ssh

---
+++ copy_ssh.py
@@ -0,0 +1,16 @@
+#/usr/bin/python
+
+from server_settings import *
+
+import os
+
+os.system("ssh-keygen")
+os.system("sudo ssh-keygen")
+
+command="ssh-copy-id "
+command+=server_username
+command+="@"
+command+=server_address
+os.system(command)
+command= "sudo "+command
+os.system(command)
